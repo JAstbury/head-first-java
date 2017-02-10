e053541a9a4d7ce713a3216e81bec6a473f91cf4
Jess Astbury <jess.astbury@hotmail.com>
2017-02-10 13:33:27 +0000

    Adds Movie class and MovieTestDrive class

---
+++ c2/Movie.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jessastbury on 10/02/2017.
+ */
+public class Movie {
+    String title;
+    String genre;
+    int rating;
+
+    void playIt() {
+        System.out.println("Playing the movie");
+    }
+}
