ae5da35941c0de6f74b47be5319b6254cf061cdb
Jess Astbury <jess.astbury@hotmail.com>
2017-02-14 15:56:27 +0000

    Improves game with ArrayList

---
+++ c6/SimpleDotComTestDrive.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+/**
+ * Created by jessastbury on 12/02/2017.
+ */
+public class SimpleDotComTestDrive {
+    public static void main(String [] args) {
+        SimpleDotCom dot = new SimpleDotCom();
+
+        ArrayList<String> locations = new ArrayList<String>();
+        locations.add("2");
+        locations.add("3");
+        locations.add("4");
+        dot.setLocationCells(locations);
+
+        String userGuess = "2";
+        String result = dot.checkYourself(userGuess);
+        String testResult = "failed";
+
+        if (result.equals("hit")) {
+            testResult = "passed";
+        }
+        System.out.println(testResult);
+    }
+}
